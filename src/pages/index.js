02e1a8e9b7ddf36429c160cacfaa04c1da177bbd
wj871287@gmail.com <wj871287@gmail.com>
2019-03-15 18:09:46 +0800

    Added the fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      {/* <Image /> */}
+      <Image />
     </div>
   </Layout>
 )
