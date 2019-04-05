06ffee2628fecb4a5bcacff984c82adebe9ed1f1
Turkyden <616287091@qq.com>
2019-04-05 21:04:59 +0800

    Fixed the gitalk

--- src/components/comment.js
+++ src/components/comment.js
@@ -8,10 +8,13 @@ class Comment extends React.Component {
     const gitalk = new Gitalk({
       clientID: '5d584e1b0f66442d84a0',
       clientSecret: '0db8efee16b70069c9b68e6033b620ae6e28a207',
-      repo: 'https://github.com/guitar-hub/guitar-hub',
-      owner: 'Turkyden',
+      repo: 'guitar-hub',
+      owner: 'guitar-hub',
       admin: ['Turkyden'],
-      title: 'GuitarHub'
+      id: window.location.pathname,
+      number: 1,
+      labels: ['learning'],
+      distractionFreeMode: false
     })
     gitalk.render('gitalk-container')
   }
