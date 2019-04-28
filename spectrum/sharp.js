d9e50c8365b9f4ba5326c1d17d7aa50b9057aaa3
wj871287@gmail.com <wj871287@gmail.com>
2019-04-28 19:22:18 +0800

    Fixed the crawler

--- spectrum/sharp.js
+++ spectrum/sharp.js
@@ -1,5 +1,16 @@
 const fs = require('fs');
 const sharp = require('sharp');
+const _ = require('lodash');
+
+const sharp1 = {
+  width: 1860,
+  height: 2631
+}
+
+const sharp2 = {
+  width: 1785,
+  height: 2525
+}
 
 /**
  * 函数-生成指定尺寸空白图层
@@ -16,31 +27,56 @@ const generateBlank = (width, height) =>
     }
   })
     .png()
-    .toBuffer()
+    .toBuffer();
 
 /**
  * 函数-合成指定图片
- * @param {*} index 
- * @param {*} base 
- * @param {*} target 
+ * @param {String} pageIndex 当前页码
+ * @param {String} base 原始路径
+ * @param {String} target 目标输出路径
  */
-const sharpImage = async (index, base, target) => 
-  sharp(base)
-    .threshold(210)
-    .composite([{
-      input: index != 0 ? await generateBlank(600, 130) : await generateBlank(600, 200),
-      top: 0,
-      left: 0,
-    }, {
-      input: await generateBlank(400, 240),
-      top: 0,
-      left: 1460
-    }, {
-      input: await generateBlank(1860, 140),
-      top: 2500,
-      left: 0 
-    }])
-    .toFile(target);
+const sharpImage = async (pageIndex, base, target) => {
+  const { width, height } = await sharp(base).metadata();
+  const size = { width, height };
+  if (_.isEqual(size, sharp1)) {
+    return sharp(base)
+      .threshold(210)
+      .composite([{
+        input: pageIndex != 0 ? await generateBlank(600, 130) : await generateBlank(600, 200),
+        top: 0,
+        left: 0,
+      }, {
+        input: pageIndex != 0 ? await generateBlank(400, 100) : await generateBlank(400, 240),
+        top: 0,
+        left: 1460
+      }, {
+        input: await generateBlank(1860, 141),
+        top: 2490,
+        left: 0 
+      }])
+      .toFile(target)
+  } else if(_.isEqual(size, sharp2)) {
+    return sharp(base)
+      .threshold(210)
+      // .composite([{
+      //   input: pageIndex != 0 ? await generateBlank(600, 130) : await generateBlank(600, 200),
+      //   top: 0,
+      //   left: 0,
+      // }, {
+      //   input: await generateBlank(400, 240),
+      //   top: 0,
+      //   left: 1460
+      // }, {
+      //   input: await generateBlank(1860, 131),
+      //   top: 2500,
+      //   left: 0 
+      // }])
+      .toFile(target)
+  } else {
+    console.log('没有对应的处理模板')
+  }
+}
+  
 
 /**
  * 函数-处理整首歌曲
@@ -50,20 +86,17 @@ const sharpSongs = async () => {
   const targetUrl = `./spectrum/classic132_sharp`;
   fs.existsSync(targetUrl) || fs.mkdirSync(targetUrl);
   const songs = fs.readdirSync(baseUrl);
-  for (let index = 0; index < songs.length; index++) {
+  for (let index = 0; index < 5; index++) {
     const song = songs[index];
     const baseSongUrl = `${baseUrl}/${song}`;
     const targetSongUrl = `${targetUrl}/${song}`;
     fs.existsSync(targetSongUrl) || fs.mkdirSync(targetSongUrl);
     const images = fs.readdirSync(baseSongUrl);
-    const info = await Promise.all(images.map((img, index)=> 
+    const info = await Promise.all(images.map((img, index) => 
       sharpImage(index, `${baseSongUrl}/${img}`, `${targetSongUrl}/${img}`)
     ))
     console.log('☑️: ' + song + '【' + info.length + '】')
   }
 }
-
-
-
 
 module.exports = sharpSongs;
