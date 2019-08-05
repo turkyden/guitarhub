4da7db84c1111615be5d0dececcb8d1311b08079
wj871287@gmail.com <wj871287@gmail.com>
2019-08-05 15:43:36 +0800

    sfsfd

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  pathPrefix: "/client",
   siteMetadata: {
     title: `Guitar Hub`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
