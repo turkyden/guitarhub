f0ae2171e17e45be15eb18401f6d1d2e941e2d96
Turkyden <616287091@qq.com>
2019-08-13 22:12:25 +0800

    Added the dish and order

--- client/src/components/rc-chord/CustomChord/utils.js
+++ client/src/components/rc-chord/CustomChord/utils.js
@@ -1,16 +1,31 @@
 export default {
+  /**
+   * 函数-获取和弦点位坐标值
+   * @param {Object} point 和弦点位
+   * @param {Object} config 和弦配置项
+   */
   getPointCoordinates(point, config) {
     return {
       x: config.origin.x + config.axisOffset.x * point.x,
       y: config.origin.y + config.axisOffset.y * point.y
     }
   },
+  /**
+   * 函数-获取和弦标注坐标值
+   * @param {Object} point 和弦点位
+   * @param {Object} config 和弦配置项
+   */
   getTextCoordinates(point, config) {
     return {
       x: config.origin.x + config.axisOffset.x * point.x,
       y: config.origin.y + config.axisOffset.y * point.y + config.textYAxisOffset
     }
   },
+  /**
+   * 函数-获取和弦横按坐标值
+   * @param {Object} point 和弦点位
+   * @param {Object} config 和弦配置项
+   */
   getCrossCoordinates(point, config) {
     return {
       leftLine: {
